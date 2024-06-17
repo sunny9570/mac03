e240d287fbaf39e0fa12f2685e70197118e235f6
sunny9570 <sundy957063@gmail.com>
2024-06-17 23:11:38 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    const headerSlide = new Swiper('.header_slide', {
+        autoplay: {
+            delay: 3000,
+            loop: true,
+
+            disableOnInteraction: false,
+        },
+    });
+})
